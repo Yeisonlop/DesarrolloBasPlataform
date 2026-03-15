d595ee9a5570c050ad80b22a1cb070814dc47e89
sebas21 <ds.sebas21@gmail.com>
2026-03-15 17:09:26 -0500

    se agregan formularios en las card de ARTISTAS-vinculacion de button para links de perfil en spotify por cada card

--- src/components/artistaCard.js
+++ src/components/artistaCard.js
@@ -4,21 +4,36 @@ import * as HoverCard from "@radix-ui/react-hover-card";
 function ArtistaCard({ artista }) {
   return (
     <HoverCard.Root>
-      {}
+
       <HoverCard.Trigger asChild>
         <div className="card-container">
-          <img src={artista.imagen} alt={artista.nombre} className="artista-img" />
 
-          {}
+          <img
+            src={artista.imagen}
+            alt={artista.nombre}
+            className="artista-img"
+          />
+
           <div className="hover-overlay">
             <h4>{artista.nombre}</h4>
+
+            <p><strong>Edad:</strong> {artista.edad}</p>
+
+            <p><strong>Género:</strong> {artista.genero}</p>
+
             <p>{artista.descripcion}</p>
+
             <div className="hover-buttons">
+              <a href={artista.spotify} target="_blank" rel="noopener noreferrer">
               <button>Play</button>
+              </a>
             </div>
+
           </div>
+
         </div>
       </HoverCard.Trigger>
+
     </HoverCard.Root>
   );
 }
--- src/paginas/artistas.js
+++ src/paginas/artistas.js
@@ -1,14 +1,68 @@
 import React from "react";
 import ArtistaCard from "../components/artistaCard";
 
-import imagen1 from "../imagenes/imagen1.jpg";
-import imagen2 from "../imagenes/imagen2.jpg";
-import imagen3 from "../imagenes/imagen3.jpg";
+import BadBunny from "../imagenes/BadBunny.jpg";
+import Drake from "../imagenes/Drake.jpg";
+import TheWeeknd from "../imagenes/TheWeeknd.jpg";
+import HarryStyles from "../imagenes/HarryStyles.jpg";
+import Feid from "../imagenes/Feid.jpg";
+import KarolG from "../imagenes/KarolG.jpg";
 
 const artistas = [
-  { nombre: "Artista 1", imagen: imagen1, descripcion: "Pop/Rock" },
-  { nombre: "Artista 2", imagen: imagen2, descripcion: "Reggaetón" },
-  { nombre: "Artista 3", imagen: imagen3, descripcion: "Electrónica" },
+  {
+    nombre: "Bad Bunny",
+    edad: 30,
+    genero: "Trap / Reggaetón",
+    descripcion: "Uno de los artistas más influyentes de la música urbana.",
+    imagen: BadBunny,
+    spotify: "https://open.spotify.com/intl-es/artist/4q3ewBCX7sLwd24euuV69X?si=ZfoAE_DLQAaNkEU_7SScLw"
+  },
+  {
+    nombre: "Drake",
+    edad: 37,
+    genero: "Hip Hop / R&B",
+    descripcion: "Artista canadiense reconocido mundialmente.",
+    imagen: Drake,
+    spotify: "https://open.spotify.com/intl-es/artist/3TVXtAsR1Inumwj472S9r4?si=jPebApXjRqOzkDz8Xv_yiA"
+  },
+  {
+    nombre: "The Weeknd",
+    edad: 34,
+    genero: "Pop / R&B",
+    descripcion: "Conocido por su estilo único y producciones innovadoras.",
+    imagen: TheWeeknd,
+    spotify: "https://open.spotify.com/intl-es/artist/1Xyo4u8uXC1ZmMpatF05PJ?si=HHw569qvT3el1thNkTqYNQ"
+  },
+
+  {
+  nombre: "Harry Styles",
+  edad: 30,
+  genero: "Pop / Pop Rock",
+  descripcion: "Cantante y compositor británico reconocido por su carrera como solista después de One Direction.",
+  imagen: HarryStyles,
+  spotify: "https://open.spotify.com/intl-es/artist/6KImCVD70vtIoJWnq6nGn3?si=ygOO9J9NQGycaRLI6c60iw"
+  },
+
+  {
+  nombre: "Feid",
+  edad: 32,
+  genero: "Reggaeton / Música Urbana",
+  descripcion: "Cantante y compositor colombiano conocido por su estilo fresco dentro del reggaetón dentro de la música urbana.",
+  imagen: Feid,
+  spotify: "https://open.spotify.com/intl-es/artist/2LRoIwlKmHjgvigdNGBHNo?si=pY6n3MgMQUSTwmQ6nk-sWw"
+
+  },
+
+  {
+  nombre: "Karol G",
+  edad: 33,
+  genero: "Reggaetón / Pop Latino",
+  descripcion: "Artista colombiana ganadora de premios internacionales y una de las voces más influyentes del género urbano.",
+  imagen: KarolG,
+  spotify: "https://open.spotify.com/intl-es/artist/790FomKkXshlbRYZFtlgla?si=Ry61E0-uSsC5wS7UtM9faQ"
+  }
+
+
 ];
 
 function Artistas() {
